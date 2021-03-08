43d343b091aa1a9ba9903bee8c5b66b1ade847cd
Ruben Gonzalez <rg@ht11.org>
2021-03-08 17:20:37 +0100

    Write all output to stderr via logger

--- app/main.py
+++ app/main.py
@@ -18,10 +18,7 @@ rants = json.load(open("data/rants.json"))
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+        log.debug('Logged in as %s [%d]', self.user.name, self.user.id)
 
     async def on_message(self, message):
         # we do not want the bot to reply to itself
